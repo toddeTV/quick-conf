9280dc628d590cede86fe78f976d3e4028fea75b
Thorsten Seyschab <business@todde.tv>
2025-10-12 15:49:34 +0200

    chore: improve project configuration and dependency management (#30)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-10-11',
+  compatibilityDate: '2025-07-15',
 
   modules: [
     '@nuxt/content',
