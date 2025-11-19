caa70d2b883ba9733e5445dc00a8feb00ef61d23
Thorsten Seyschab <business@todde.tv>
2025-11-19 14:07:50 +0900

    feat: add example content for AppConfig footer configuration

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,4 +1,52 @@
+/**
+ * Application configuration file.
+ * Schema is defined in `nuxt.schema.ts`.
+ */
 export default defineAppConfig({
+  socials: {
+    social1: {
+      name: 'toddeTV',
+      icon: 'i-simple-icons-x',
+      url: 'https://x.com/toddeTV',
+    },
+    social2: {
+      name: 'toddeTV',
+      icon: 'i-simple-icons-bluesky',
+      url: 'https://bsky.app/profile/todde.tv',
+    },
+    social3: {
+      name: 'toddeTV',
+      icon: 'i-simple-icons-linkedin',
+      url: 'https://www.linkedin.com/in/toddetv/',
+    },
+    social4: {
+      name: 'toddeTV',
+      icon: 'i-simple-icons-github',
+      url: 'https://github.com/toddeTV/',
+    },
+  },
+
+  customFooterColumn: {
+    title: 'About This Conference',
+    links: {
+      link1: {
+        name: 'some link without icon',
+        // icon: 'i-lucide-house',
+        url: '/',
+      },
+      link2: {
+        name: 'some link with icon',
+        icon: 'i-lucide-house',
+        url: '/',
+      },
+      link3: {
+        name: 'external links also work',
+        icon: 'i-simple-icons-x',
+        url: 'https://x.com/toddeTV',
+      },
+    },
+  },
+
   ui: { // for `@nuxt/ui`
   },
 })
