def3866e56aa7ffdd4ae496f94827922db4a13cc
Thorsten Seyschab <business@todde.tv>
2025-11-24 20:44:15 +0100

    feat: add color mode aware favicon to Nuxt header

--- app/app.vue
+++ app/app.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 const colorMode = useColorMode()
+const appConfig = useAppConfig()
+const { autoSwitchOnColorMode } = useImgPaths()
 
 const color = computed(() => colorMode.value === 'dark' ? '#020618' : '#ffffff')
 
-useHead({
+useHead(() => ({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color },
+    { key: 'theme-color', name: 'theme-color', content: color }, // TODO used or delete?
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' },
+    {
+      rel: 'icon',
+      // href: '/favicon.svg',
+      href: autoSwitchOnColorMode({
+        dark: appConfig.general?.favicon?.dark,
+        light: appConfig.general?.favicon?.light,
+      }),
+    },
   ],
   htmlAttrs: {
     lang: 'en',
   },
-})
+}))
 
 useSeoMeta({
   // titleTemplate: '%s',
