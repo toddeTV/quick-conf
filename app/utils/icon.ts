cfce67efed3c26d824854875e7d30864fd44c7cb
Thorsten Seyschab <business@todde.tv>
2025-11-27 18:09:22 +0100

    feat: improve hostname comparison to protect against spoofing

--- app/utils/icon.ts
+++ app/utils/icon.ts
@@ -2,8 +2,6 @@
  * @file Icon related utility functions.
  */
 
-import { endsWith } from 'lodash-es'
-
 /**
  * Returns a Simple Icons icon name for a given URL.
  *
@@ -17,54 +15,54 @@ import { endsWith } from 'lodash-es'
 export function getIconForUrl(url: string): string {
   try {
     const { hostname } = new URL(url)
-    const normalizedHostname = hostname.replace(/^www\./, '')
+    const normalizedHostname = hostname.replace(/^www\./, '').toLowerCase()
 
     // Social & Professional Networks
-    if (endsWith(normalizedHostname, 'github.com'))
+    if (normalizedHostname === 'github.com')
       return 'i-simple-icons-github'
-    if (endsWith(normalizedHostname, 'twitter.com') || endsWith(normalizedHostname, 'x.com'))
+    if (normalizedHostname === 'twitter.com' || normalizedHostname === 'x.com')
       return 'i-simple-icons-x'
-    if (endsWith(normalizedHostname, 'bsky.app'))
+    if (normalizedHostname === 'bsky.app')
       return 'i-simple-icons-bluesky'
-    if (endsWith(normalizedHostname, 'linkedin.com'))
+    if (normalizedHostname === 'linkedin.com')
       return 'i-simple-icons-linkedin'
-    if (endsWith(normalizedHostname, 'youtube.com'))
+    if (normalizedHostname === 'youtube.com')
       return 'i-simple-icons-youtube'
-    if (endsWith(normalizedHostname, 'facebook.com'))
+    if (normalizedHostname === 'facebook.com')
       return 'i-simple-icons-facebook'
-    if (endsWith(normalizedHostname, 'instagram.com'))
+    if (normalizedHostname === 'instagram.com')
       return 'i-simple-icons-instagram'
-    if (endsWith(normalizedHostname, 'mastodon.social'))
+    if (normalizedHostname === 'mastodon.social')
       return 'i-simple-icons-mastodon'
-    if (endsWith(normalizedHostname, 'medium.com'))
+    if (normalizedHostname === 'medium.com')
       return 'i-simple-icons-medium'
-    if (endsWith(normalizedHostname, 'dev.to'))
+    if (normalizedHostname === 'dev.to')
       return 'i-simple-icons-devdotto'
-    if (endsWith(normalizedHostname, 'behance.net'))
+    if (normalizedHostname === 'behance.net')
       return 'i-simple-icons-behance'
-    if (endsWith(normalizedHostname, 'dribbble.com'))
+    if (normalizedHostname === 'dribbble.com')
       return 'i-simple-icons-dribbble'
-    if (endsWith(normalizedHostname, 'codepen.io'))
+    if (normalizedHostname === 'codepen.io')
       return 'i-simple-icons-codepen'
-    if (endsWith(normalizedHostname, 'stackoverflow.com'))
+    if (normalizedHostname === 'stackoverflow.com')
       return 'i-simple-icons-stackoverflow'
-    if (endsWith(normalizedHostname, 'thingiverse.com'))
+    if (normalizedHostname === 'thingiverse.com')
       return 'i-simple-icons-thingiverse'
-    if (endsWith(normalizedHostname, 'printables.com'))
+    if (normalizedHostname === 'printables.com')
       return 'i-simple-icons-printables'
-    if (endsWith(normalizedHostname, 'artstation.com'))
+    if (normalizedHostname === 'artstation.com')
       return 'i-simple-icons-artstation'
-    if (endsWith(normalizedHostname, 'tiktok.com'))
+    if (normalizedHostname === 'tiktok.com')
       return 'i-simple-icons-tiktok'
-    if (endsWith(normalizedHostname, 'twitch.tv'))
+    if (normalizedHostname === 'twitch.tv')
       return 'i-simple-icons-twitch'
-    if (endsWith(normalizedHostname, 'discord.gg') || endsWith(normalizedHostname, 'discord.com'))
+    if (normalizedHostname === 'discord.gg' || normalizedHostname === 'discord.com')
       return 'i-simple-icons-discord'
-    if (endsWith(normalizedHostname, 'reddit.com'))
+    if (normalizedHostname === 'reddit.com')
       return 'i-simple-icons-reddit'
-    if (endsWith(normalizedHostname, 'pinterest.com'))
+    if (normalizedHostname === 'pinterest.com')
       return 'i-simple-icons-pinterest'
-    if (endsWith(normalizedHostname, 'gitlab.com'))
+    if (normalizedHostname === 'gitlab.com')
       return 'i-simple-icons-gitlab'
   }
   catch {
