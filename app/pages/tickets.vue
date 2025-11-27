f7eabd7731ebe271297e20951e0ec525786de97f
Thorsten Seyschab <business@todde.tv>
2025-11-27 20:26:01 +0100

    feat: Add ticket page (#51)

--- app/pages/tickets.vue
+++ app/pages/tickets.vue
@@ -1,8 +1,39 @@
 <script setup lang="ts">
+const { extractSeoMetadata, getSeoMetaBase } = useSeo()
+
+const { data: tickets } = await useAsyncData('tickets-all', () => queryCollection('tickets').all())
+
+const seoMetadata = extractSeoMetadata({
+  title: 'Tickets',
+  description: 'Purchase your ticket to join the conference.',
+})
+const { title, description } = seoMetadata
+
+useSeoMeta({
+  ...getSeoMetaBase(seoMetadata),
+})
 </script>
 
 <template>
-  <UContainer>
-    Tickets
-  </UContainer>
+  <template v-if="tickets">
+    <UContainer>
+      <UBreadcrumb
+        class="pt-3"
+        :items="[
+          { label: 'Home', to: '/' },
+          { label: 'Tickets' },
+        ]"
+      />
+
+      <UPageHeader
+        :description="description"
+        :title="title"
+        :ui="{
+          root: 'border-b-0!',
+        }"
+      />
+
+      <UPricingPlans :plans="tickets" />
+    </UContainer>
+  </template>
 </template>
