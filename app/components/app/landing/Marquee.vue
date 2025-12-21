78307eef36ec53df21485d07fc36bf31a4abd4cb
Thorsten Seyschab <business@todde.tv>
2025-12-21 23:45:15 +0100

    feat: add landing page block `Marquee`

---
+++ app/components/app/landing/Marquee.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  images: {
+    src: string
+    alt?: string
+  }[]
+  direction?: 'left' | 'right'
+  speed?: number // duration in seconds
+  gap?: number // gap in px
+}>(), {
+  direction: 'left',
+  speed: 20,
+  gap: 0,
+})
+
+const style = computed(() => ({
+  '--marquee-duration': `${props.speed}s`,
+  '--marquee-gap': `${props.gap}px`,
+}))
+</script>
+
+<template>
+  <div class="relative flex overflow-hidden py-8" :style="style">
+    <div
+      class="flex shrink-0 items-center justify-start gap-(--marquee-gap) animate-marquee"
+      :class="{ 'animate-marquee-reverse': direction === 'right' }"
+    >
+      <NuxtImg
+        v-for="(image, index) in images"
+        :key="index"
+        :alt="image.alt"
+        class="max-w-none h-32 object-contain"
+        :src="image.src"
+      />
+    </div>
+    <div
+      aria-hidden="true"
+      class="flex shrink-0 items-center justify-start gap-(--marquee-gap) animate-marquee
+      ml-(--marquee-gap)"
+      :class="{ 'animate-marquee-reverse': direction === 'right' }"
+    >
+      <NuxtImg
+        v-for="(image, index) in images"
+        :key="`clone-${index}`"
+        :alt="image.alt"
+        class="max-w-none h-32 object-contain"
+        :src="image.src"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.animate-marquee {
+  animation: marquee var(--marquee-duration) linear infinite;
+}
+
+.animate-marquee-reverse {
+  animation-direction: reverse;
+}
+
+@keyframes marquee {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(calc(-100% - var(--marquee-gap)));
+  }
+}
+</style>
