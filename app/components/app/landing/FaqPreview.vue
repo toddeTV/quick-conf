21d42e1bcce70fb7635ad786199d3e4dbcf31d33
Thorsten Seyschab <business@todde.tv>
2025-12-21 23:25:41 +0100

    feat: add landing page block `FaqPreview`

---
+++ app/components/app/landing/FaqPreview.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
+defineProps<{
+  title: string
+  description: string
+  headline: string
+  items: {
+    label: string
+    content: string
+  }[]
+  link?: ButtonProps
+}>()
+</script>
+
+<template>
+  <UPageSection
+    :description="description"
+    :headline="headline"
+    :title="title"
+  >
+    <div class="sm:px-10 lg:px-24">
+      <UAccordion :items="items" />
+
+      <div v-if="link" class="mt-10 flex justify-center">
+        <UButton v-bind="link" />
+      </div>
+    </div>
+  </UPageSection>
+</template>
