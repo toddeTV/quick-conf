2ac33c7dd4e0b2a739723127449c03c57d2df1ef
Thorsten Seyschab <business@todde.tv>
2025-12-21 16:39:44 +0100

    feat: implement 'View all Sponsors' button on landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -142,6 +142,7 @@ useSeoMeta({
         <UPageCard
           v-for="(sponsor, index) in sponsors"
           :key="index"
+          class="flex h-32 items-center justify-center text-center"
           rel="noopener noreferrer"
           spotlight
           target="_blank"
@@ -151,10 +152,28 @@ useSeoMeta({
           <template #default>
             <NuxtImg
               :alt="`Logo of sponsor ${sponsor.slug}`"
-              class="mx-auto h-12 w-full object-contain dark:invert"
+              class="mx-auto h-20 w-full object-contain dark:invert"
               :src="sponsor.image"
             />
           </template>
+        </UPageCard>
+
+        <UPageCard
+          v-if="page.sponsors.showViewAll"
+          class="flex h-32 items-center justify-center text-center"
+          spotlight
+          :to="page.sponsors.viewAllLink"
+          variant="subtle"
+        >
+          <div class="flex flex-col items-center gap-y-2">
+            <UIcon
+              class="text-4xl"
+              name="i-lucide-handshake"
+            />
+            <span class="text-lg font-medium text-gray-900 dark:text-white">
+              View All Sponsors
+            </span>
+          </div>
         </UPageCard>
       </UPageGrid>
     </UPageSection>
