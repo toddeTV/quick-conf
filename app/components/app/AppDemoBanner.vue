09e47125be86c165ab6cb68f064fa1a8dfd8585a
Thorsten Seyschab <business@todde.tv>
2026-01-29 01:02:04 +0100

    feat: better legal disclaimer text

--- app/components/app/AppDemoBanner.vue
+++ app/components/app/AppDemoBanner.vue
@@ -8,14 +8,28 @@ const isDemoMode = computed(() => {
 
 <template>
   <div
-    v-if="isDemoMode "
-    class="h-14 w-full"
+    v-if="isDemoMode"
+    class="h-16 w-full"
   >
     <div
-      class="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-red-600
-      p-3 text-center text-sm font-bold text-white shadow-lg"
+      class="fixed bottom-0 left-0 right-0 z-100 flex flex-col items-center justify-center
+      gap-2 bg-red-600 p-3 text-center text-xs text-white shadow-lg md:flex-row lg:text-sm"
     >
-      <span>⚠️ This is a demo website with dummy data. No real content.</span>
+      <span>
+        ⚠️ <u>LEGAL DISCLAIMER:</u> This is a technical demonstration of a website template.
+        It does not represent a real website. All content, including Legal Notice and Privacy Policy, is fictitious.
+        No binding agreements or purchases can be made.
+      </span>
+      <UButton
+        class="w-32 shrink-0 flex justify-center"
+        color="neutral"
+        size="xs"
+        target="_blank"
+        to="https://github.com/toddeTV/quick-conf"
+        variant="solid"
+      >
+        View Source Code
+      </UButton>
     </div>
   </div>
 </template>
