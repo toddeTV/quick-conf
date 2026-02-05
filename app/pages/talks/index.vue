6a72f027b0c67949cfe37ca9b310d37f04ceff12
Thorsten Seyschab <business@todde.tv>
2026-02-05 09:31:18 +0100

    refactor: set explicit redirect code on redirect

--- app/pages/talks/index.vue
+++ app/pages/talks/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-await navigateTo({
-  path: '/schedule',
+// await navigateTo({
+//   path: '/schedule',
+// })
+await navigateTo('/schedule', {
+  redirectCode: 302,
 })
 </script>
