825fbc10c3a86792c42e423ff3ceb1b1eab928fb
Thorsten Seyschab <business@todde.tv>
2026-02-05 22:10:32 +0100

    feat: change design of demo legal disclaimer banner

--- app/components/app/AppDemoBanner.vue
+++ app/components/app/AppDemoBanner.vue
@@ -9,27 +9,23 @@ const isDemoMode = computed(() => {
 <template>
   <div
     v-if="isDemoMode"
-    class="h-16 w-full"
+    class="flex w-full flex-col items-center justify-center gap-2 border-t border-red-200 bg-red-50 p-3 text-center
+    text-xs text-red-800 dark:border-red-900 dark:bg-red-950 dark:text-red-200 md:flex-row lg:text-sm"
   >
-    <div
-      class="fixed bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center
-      gap-2 bg-red-600 p-3 text-center text-xs text-white shadow-lg md:flex-row lg:text-sm"
+    <span>
+      ⚠️ <u>Legal disclaimer:</u> This is a technical demonstration of a website template.
+      It does not represent a real website. All content, including Legal Notice and Privacy Policy, is fictitious.
+      No binding agreements or purchases can be made.
+    </span>
+    <UButton
+      class="w-32 shrink-0 flex justify-center"
+      color="neutral"
+      size="xs"
+      target="_blank"
+      to="https://github.com/toddeTV/quick-conf"
+      variant="solid"
     >
-      <span>
-        ⚠️ <u>LEGAL DISCLAIMER:</u> This is a technical demonstration of a website template.
-        It does not represent a real website. All content, including Legal Notice and Privacy Policy, is fictitious.
-        No binding agreements or purchases can be made.
-      </span>
-      <UButton
-        class="w-32 shrink-0 flex justify-center"
-        color="neutral"
-        size="xs"
-        target="_blank"
-        to="https://github.com/toddeTV/quick-conf"
-        variant="solid"
-      >
-        View Source Code
-      </UButton>
-    </div>
+      View Source Code
+    </UButton>
   </div>
 </template>
